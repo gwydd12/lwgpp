9f23e64c25a621f03c3a847b330f2ae5896918bd
gwydd <gwydd12@proton.me>
2026-01-04 21:00:09 +0100

    refactor: minor changes in memory

--- src/memory/TrackingMemoryResource.h
+++ src/memory/TrackingMemoryResource.h
@@ -17,45 +17,13 @@ namespace memory {
         explicit TrackingMemoryResource(memory_resource* parent = std::pmr::get_default_resource())
             : parent_(parent) {}
 
-        [[nodiscard]] Stats get_stats() const noexcept {
-            return Stats{
-            total_bytes_allocated_.load(),
-                alloc_calls_.load(),
-                current_bytes_.load(),
-                peak_bytes_.load()
-            };
-        }
-
-        void reset_stats() noexcept {
-            total_bytes_allocated_ = 0;
-            alloc_calls_ = 0;
-            current_bytes_ = 0;
-            peak_bytes_ = 0;
-        }
+        Stats get_stats() const noexcept;
+        void reset_stats() noexcept;
 
     protected:
-        void* do_allocate(const std::size_t bytes, const std::size_t alignment) override {
-            void* p = parent_->allocate(bytes, alignment);
-            total_bytes_allocated_ += bytes;
-            ++alloc_calls_;
-            current_bytes_ += bytes;
-            if (current_bytes_.load() > peak_bytes_.load()) {
-                peak_bytes_ = current_bytes_.load();
-            }
-            return p;
-        }
-
-        void do_deallocate(void* p, const std::size_t bytes, const std::size_t alignment) override {
-            parent_->deallocate(p, bytes, alignment);
-            current_bytes_ -= bytes;
-        }
-
-        [[nodiscard]] bool do_is_equal(const memory_resource& other) const noexcept override {
-            if (const auto* o = dynamic_cast<const TrackingMemoryResource*>(&other)) {
-                return parent_->is_equal(*o->parent_);
-            }
-            return false;
-        }
+        void* do_allocate(std::size_t bytes, std::size_t alignment) override;
+        void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override;
+        bool do_is_equal(const memory_resource& other) const noexcept override;
 
     private:
         memory_resource* parent_;
